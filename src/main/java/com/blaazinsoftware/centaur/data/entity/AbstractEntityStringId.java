c109b4aeb4af000996ae3852d0acea97a25a6ced
randymay <randy.may@blaazinsoftware.com>
2016-05-31 06:44:06 -0400

    Allow for String-based IDs.

---
+++ src/main/java/com/blaazinsoftware/centaur/data/entity/AbstractEntityStringId.java
@@ -0,0 +1,52 @@
+package com.blaazinsoftware.centaur.data.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.googlecode.objectify.annotation.Entity;
+import com.googlecode.objectify.annotation.Id;
+
+import java.io.Serializable;
+
+/**
+ * Convenient base class for using Centaur.
+ */
+@Entity
+public abstract class AbstractEntityStringId implements Serializable {
+
+    @Id
+    @JsonIgnore
+    private String id;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        if (null == id) {
+            return "null";
+        }
+        return "ID: " + id;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof AbstractEntityStringId) {
+            AbstractEntityStringId entity = (AbstractEntityStringId)obj;
+            return !(id == null || entity.getId() == null) && (id.equals(entity.getId()));
+        }
+
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        if (id == null) {
+            return 0;
+        }
+        return id.hashCode();
+    }
+}
